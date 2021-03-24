73188b695ff6b00955bb1993fe08cc4ce263d7df
Paula Nerenberg <paulita.glam@gmail.com>
2021-03-24 12:52:00 +0000

    Previous version restored again

--- pages/__tests__/Custom404.test.js
+++
@@ -1,6 +0,0 @@
-import { shallow } from "enzyme";
-import Custom404 from "../Custom404";
-
-it("expect to render Custom404 component", () => {
-  expect(shallow(<Custom404 />).length).toEqual(1);
-});
